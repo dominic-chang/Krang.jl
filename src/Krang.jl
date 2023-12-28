c1b4a6e7949595c00a1af53f3fe9675e4c48a84e
Dominic <dchang3419@hotmail.com>
2023-12-28 15:02:26 -0500

    Update Compatibility
    * Updated Julia compatibility to 1.10
    * Refactored folders to match style guide

--- src/Krang.jl
+++ src/Krang.jl
@@ -10,15 +10,15 @@ using JacobiElliptic
 
 # Write your package code here.
 include("metrics/AbstractMetric.jl")
-include("metrics/Kerr.jl")
-include("Geometries/geometry_types.jl")
-include("Cameras/camera_types.jl")
-include("Cameras/SlowLightIntensityCamera.jl")
-include("Cameras/IntensityCamera.jl")
-include("Kerr/raytracer.jl")
-include("Kerr/api.jl")
-include("Materials/material_types.jl")
-include("Materials/PowerLawPolarization.jl")
-include("Materials/observations.jl")
+include("metrics/Kerr/Kerr.jl")
+include("geometries/geometry_types.jl")
+include("cameras/camera_types.jl")
+include("cameras/SlowLightIntensityCamera.jl")
+include("cameras/IntensityCamera.jl")
+include("metrics/Kerr/raytracer.jl")
+include("metrics/Kerr/api.jl")
+include("materials/material_types.jl")
+include("materials/PowerLawPolarization.jl")
+include("materials/observations.jl")
 
 end
