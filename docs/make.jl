55dcd08acfe0639e3dd378b4478f6c762a2c17b8
Dominic <dchang3419@hotmail.com>
2024-03-03 12:18:02 -0500

    Update Documentation
    Migrated documentation renderer to DocumentationVitepress

--- docs/make.jl
+++ docs/make.jl
@@ -1,5 +1,6 @@
 using Krang
 using Documenter
+using DocumenterVitepress
 using Glob
 using Literate
 
@@ -7,11 +8,12 @@ using Literate
 GENERATED = joinpath(@__DIR__, "..", "examples")
 OUTDIR = joinpath(@__DIR__, "src", "examples")
 
-SOURCE_FILES = Glob.glob("*.jl", GENERATED)
+blacklist = ["gpu", "JuKeBOX", "Krang_logo", "enzyme"]
+SOURCE_FILES = filter(x-> all(i->!occursin(i, x), blacklist), Glob.glob("*.jl", GENERATED))[1:2]
 foreach(fn -> Literate.markdown(fn, OUTDIR, documenter=true), SOURCE_FILES)
-MD_FILES = filter(x-> !occursin("gpu", x), [joinpath("examples", file) for file in readdir(OUTDIR)])
+MD_FILES = [joinpath("examples", file) for file in readdir(OUTDIR)]
 
-DocMeta.setdocmeta!(Krang, :DocTestSetup, :(using Krang); recursive=true)
+Documenter.DocMeta.setdocmeta!(Krang, :DocTestSetup, :(using Krang); recursive=true)
 
 format = Documenter.HTML(;
         prettyurls=get(ENV, "CI", "false") == "true",
@@ -19,17 +21,24 @@ format = Documenter.HTML(;
         edit_link="main",
         assets=String[],
     )
+format=DocumenterVitepress.MarkdownVitepress(
+    repo = "https://dchang10.github.io/Krang.jl", # this must be the full URL!
+    devbranch = "main",
+    devurl = "dev",
+    #clean_md_output = true
+    ;
+)
 
 makedocs(;
-    modules=[Krang],
-    authors="Dominic <dchang3419@hotmail.com> and contributors",
-    repo="https://github.com/dchang10/Krang.jl/blob/{commit}{path}#{line}",
     sitename="Krang.jl",
+    authors="Dominic <dchang3419@hotmail.com> and contributors",
+    modules=[Krang],
+    repo="https://github.com/dchang10/Krang.jl/blob/{commit}{path}#{line}",
     format=format,
     pages=[
         "Home" => "index.md",
         "Getting Started" => "getting_started.md",
-        "Examples" => MD_FILES,
+        #"Examples" => MD_FILES,
         "Theory" => [
             "Raytracing" => [
                 "kerr_geodesic_summary.md",
--- docs/src/getting_started.md
+++ docs/src/getting_started.md
@@ -2,7 +2,7 @@
 
 Light rays in this module can be parameterized in terms of either the cones ($\theta_s$), or the minotime ($\Delta\tau$).
 
-## Raytracing conical surfaces ($\theta_s$)
+## Raytracing conical surfaces
 Surfaces of constant $\theta$ define spin axis centered cones whose apex lie at the origin of the coordinate system.
 
 ![raytracing conical surfaces](examples/coordinate.gif)
@@ -10,7 +10,7 @@ Surfaces of constant $\theta$ define spin axis centered cones whose apex lie at 
 <p style="text-align:center">n=0 and n=1 images of emission coordinates originating from conical surfaces.</p>
 ```
 
-## Raytracing with rays parameterized by Mino time ($\Delta\tau$)
+## Raytracing with rays parameterized by Mino time
 
 ![raytracing with Mino time](examples/raytrace.gif)
 ```@raw html
