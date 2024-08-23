43e8e818b264a3e8cd18a41badcd126dabd59bd2
dominic-chang <dochang@g.harvard.edu>
2024-08-23 14:29:08 -0400

    Add Mesh Geometry Type

---
+++ test/mesh_geometry_tests.jl
@@ -0,0 +1,10 @@
+@testset "Mesh Geometry Manipulation" begin
+    bunny = load(joinpath((@__DIR__),"stanfordbunny.obj") )
+    rot = Rotations.AngleAxis(π / 2, 0.0, 0.0, 1.0)
+    points = (Ref(rot) .* (bunny.position .* 150)) .+   Ref([15, 3, 0])
+    faces = getfield(getfield(bunny, :simplices), :faces)
+    bunny_mesh1 = GeometryBasics.Mesh([Point(x) for x in points], faces)
+    
+    bunny_mesh = translate(rotate(scale(bunny, 150), π / 2, 0.0, 0.0, 1.0), 15, 3, 0)
+    @test all(map(x-> x[1] == x[2],zip(bunny_mesh.position, bunny_mesh1.position)))
+end
