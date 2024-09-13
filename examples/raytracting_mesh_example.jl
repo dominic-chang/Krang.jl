59c73ed918920f9a153320af3165ba17e68b69b4
dominic-chang <dochang@g.harvard.edu>
2024-09-13 00:06:04 -0400

    Update bunny position

--- examples/raytracting_mesh_example.jl
+++ examples/raytracting_mesh_example.jl
@@ -6,8 +6,8 @@ using FileIO
 
 spin = 0.99
 metric = Krang.Kerr(spin)
-θo = 40/180*π
-ρmax = 20.0
+θo = 90/180*π
+ρmax = 22.0
 sze = 200
 
 # Import external mesh
@@ -15,12 +15,12 @@ bunny_mesh = translate(
     rotate(
         scale(
             load(download("https://graphics.stanford.edu/~mdfisher/Data/Meshes/bunny.obj", "bunny.obj")), 150
-            ), π / 2, 0.0, 0.0, 1.0
-        ), 15, 3, 0
+            ), 3π / 2, 0.0, 0.0, 1.0
+        ), -10, -4, 0
     )
 
 fig = CMk.Figure()
-ax = CMk.Axis3(fig[1,1], aspect=(1,1,1), elevation = 130/180*π, azimuth = 0.0, zreversed=true, xreversed=false)
+ax = CMk.Axis3(fig[1,1], aspect=(1,1,1), elevation = π/2, azimuth = π)
 CMk.mesh!(ax, bunny_mesh, color=[parse(CMk.Colorant, "rgba(0%, 50%, 70%,1.0)") for tri in bunny_mesh.position], colormap = :blues, transparency=true, shading=true)
 fig
 save("stanford_bunny.png", fig)
@@ -30,9 +30,9 @@ save("stanford_bunny.png", fig)
 # Raytrace the mesh embedded in the Kerr space time with additive blending
 camera = Krang.IntensityCamera(metric, θo, -ρmax, ρmax, -ρmax, ρmax, sze)
 
-intersections = raytrace(camera, bunny_mesh)
+intersections = raytrace(camera, bunny_mesh)'
 fig = CMk.Figure()
-ax = CMk.Axis(fig[1,1], aspect=1, xreversed=true, yreversed=true)    
+ax = CMk.Axis(fig[1,1], aspect=1, xreversed=true)  
 CMk.heatmap!(ax, intersections)
 fig
 
