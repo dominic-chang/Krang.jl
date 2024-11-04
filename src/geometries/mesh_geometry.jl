2907b98dca924242f581135031d1af299de85ff8
dominic-chang <dochang@g.harvard.edu>
2024-11-04 08:06:55 -0500

    Refactor mesh_geometry.jl to use fully qualified names for Point and Mesh

--- src/geometries/mesh_geometry.jl
+++ src/geometries/mesh_geometry.jl
@@ -14,18 +14,18 @@ const MeshGeometry = GeometryBasics.Mesh
 function translate(mesh::MeshGeometry, x, y, z)
     points = (Ref([x, y, z]) .+ mesh.position)
     faces = getfield(getfield(mesh, :simplices), :faces)
-    return GeometryBasics.Mesh([Point(x) for x in points], faces)
+    return GeometryBasics.Mesh([GeometryBasics.Point(x) for x in points], faces)
 end
 
 function scale(mesh::MeshGeometry, multiple)
     points = (multiple .* mesh.position)
     faces = getfield(getfield(mesh, :simplices), :faces)
-    return GeometryBasics.Mesh([Point(x) for x in points], faces)
+    return GeometryBasics.Mesh([GeometryBasics.Point(x) for x in points], faces)
 end
 
 function rotate(mesh::MeshGeometry, angle, x, y, z)
     angleaxis = Rotations.AngleAxis(angle, x, y, z)
     points = Ref(angleaxis) .* (mesh.position)
     faces = getfield(getfield(mesh, :simplices), :faces)
-    return GeometryBasics.Mesh([Point(x) for x in points], faces)
+    return GeometryBasics.Mesh([GeometryBasics.Point(x) for x in points], faces)
 end
