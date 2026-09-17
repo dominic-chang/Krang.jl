cc0700a7c205abe0529aefb8c9a34f42288663db
dominic-chang <dochang@g.harvard.edu>
2026-09-17 01:56:41 -0400

    Fix type definition for ray vector in generate_ray function

--- src/schemes/RayTrace.jl
+++ src/schemes/RayTrace.jl
@@ -19,7 +19,8 @@ function generate_ray!(
 end
 
 function generate_ray(pixel::AbstractPixel, res::Int)
-    ray = Vector{Intersection}(undef, res)#zeros(T, 3, res)
+    T = typeof(metric(pixel).spin)
+    ray = Vector{Intersection{T}}(undef, res)
     generate_ray!(ray, pixel, res)
     return ray
 end
